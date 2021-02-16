acf7d28d4592dc31e0347607cf821a93d66d27c7
Martin Randall <martin@coryl.com>
2021-02-16 17:09:01 +0000

    Added image and test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'svg-colour-test';
+export class AppComponent implements OnInit {
+  rotations = [];
+
+  ngOnInit() {
+    for (let i = 0; i < 180; i++) {
+      this.rotations.push(i * 2);
+    }
+  }
 }
